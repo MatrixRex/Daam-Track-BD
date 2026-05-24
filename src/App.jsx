b6559990f383d55f8de93f78ed4d86fc8d3ab6ca
MatrixRex <matrixrex05@gmail.com>
2026-05-25 00:33:46 +0600

    add top padding to item list container to prevent selection ring clipping

--- src/App.jsx
+++ src/App.jsx
@@ -437,7 +437,7 @@ function App() {
               </div>
             </div>
             
-            <div className="flex-1 min-h-0 overflow-y-auto custom-scrollbar pr-2">
+            <div className="flex-1 min-h-0 overflow-y-auto custom-scrollbar pr-2 pt-1.5">
                 <StatsSidebar
                   items={sortedItems}
                   stats={Object.values(itemStats)}
